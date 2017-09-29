7ce2bbbce77126e1fc98939342691b0928713dc2
tAndreani <andreani.tommaso86@gmail.com>
2017-09-29 11:36:28 +0200

    Added additional step of sorting the files

--- Bash/Create.BigWig.sh
+++ Bash/Create.BigWig.sh
@@ -12,9 +12,9 @@ cat Gadd45.tko3.fast.bismark.cov.bed | awk '$5 >= 10' > Gadd45.tko3.fast.bismark
 cat mESC.bismark.cov.bed | awk '$5 >= 10' > mESC.bismark.cov.major.egual.10.bed &
 
 
-#Download wigToGiWig
+#Download bedGraphToBigWig
 
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig
+wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bedGraphToBigWig
 
 
 #Convert the files in wig format #chr, start, end, meth	values
@@ -22,6 +22,12 @@ for i in *egual.10.bed;
 do
         cut -f 1,2,3,4 $i > $i.bedgraph; 
 done &
+
+## Sort he files
+LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko1.bismark.cov.major.egual.10.sort.bedgraph Gadd45.tko1.bismark.cov.major.egual.10.bedgraph
+LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko2.fast.bismark.cov.major.egual.10.sort.bedgraph Gadd45.tko2.fast.bismark.cov.major.egual.10.bedgraph
+LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko3.fast.bismark.cov.major.egual.10.sort.bedgraph Gadd45.tko3.fast.bismark.cov.major.egual.10.bedgraph
+LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o mESC.bismark.cov.major.egual.10.sort.bedgraph mESC.bismark.cov.major.egual.10.bedgraph
 
 
 #Convert to BigWig
