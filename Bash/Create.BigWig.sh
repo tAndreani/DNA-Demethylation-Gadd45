5031ec7464ba075a1b4af99106945b98080d704d
tAndreani <andreani.tommaso86@gmail.com>
2017-10-02 18:07:32 +0200

    Create new coverage files and update with thrs >= 5

--- Bash/Create.BigWig.sh
+++ Bash/Create.BigWig.sh
@@ -1,15 +1,18 @@
-#Compute the coverage
-zcat Gadd45.tko1.bismark.cov.gz | awk -F "\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5+$6}' > Gadd45.tko1.bismark.cov.bed &
-zcat Gadd45.tko2.bismark.cov.gz | awk -F "\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5+$6}' > Gadd45.tko2.bismark.cov.bed &
-zcat Gadd45.tko3.bismark.cov.gz | awk -F "\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5+$6}' > Gadd45.tko3.bismark.cov.bed &
-zcat mESC.bismark.cov.gz | awk -F "\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5+$6}' > mESC.bismark.cov.bed &
-
+#From the methylation extracted files (all of them with 43735674 CG extracted ) create a coverage file
+zcat ES.WT.myCpG.gz | awk '{OFS="\t";if($4+0 > 0 || $5+0 >0 ) print $1"\t"$2-1"\t"$2"\t"$4/($4+$5),
+$4+$5;}' > ES.WT.myCpG.bed &
+zcat G45.TKO1.myCpG.gz | awk '{OFS="\t";if($4+0 > 0 || $5+0 >0 ) print $1"\t"$2-1"\t"$2"\t"$3"\t"$4/($4+$5),
+$4+$5;}' > G45.TKO1.myCpG.bed &
+zcat G45.TKO2.myCpG.gz | awk '{OFS="\t";if($4+0 > 0 || $5+0 >0 ) print $1"\t"$2-1"\t"$2"\t"$3"\t"$4/($4+$5),
+$4+$5;}' > G45.TKO2.myCpG.bed &
+zcat G45.TKO3.myCpG.gz | awk '{OFS="\t";if($4+0 > 0 || $5+0 >0 ) print $1"\t"$2-1"\t"$2"\t"$3"\t"$4/($4+$5),
+$4+$5;}' > G45.TKO3.myCpG.bed &
 
 #Filter CG with Coverage >= 10
-cat Gadd45.tko1.bismark.cov.bed | awk '$5 >= 10' > Gadd45.tko1.bismark.cov.major.egual.10.bed &
-cat Gadd45.tko2.bismark.cov.bed | awk '$5 >= 10' > Gadd45.tko2.bismark.cov.major.egual.10.bed &
-cat Gadd45.tko3.bismark.cov.bed | awk '$5 >= 10' > Gadd45.tko3.bismark.cov.major.egual.10.bed &
-cat mESC.bismark.cov.bed | awk '$5 >= 10' > mESC.bismark.cov.major.egual.10.bed &
+cat ES.WT.myCpG.bed | awk '$5 >= 10' > ES.WT.cov.major.egual.10.bed &
+cat G45.TKO1.myCpG.bed | awk '$5 >= 10' > G45.TKO1.cov.major.egual.10.bed &
+cat G45.TKO2.myCpG.bed | awk '$5 >= 10' > G45.TKO2.cov.major.egual.10.bed &
+cat G45.TKO3.myCpG.bed | awk '$5 >= 10' > G45.TKO3.cov.major.egual.10.bed &
 
 
 #Download bedGraphToBigWig
@@ -23,21 +26,38 @@ do
         cut -f 1,2,3,4 $i > $i.bedgraph; 
 done &
 
-## Sort he files
-LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko1.bismark.cov.major.egual.10.bed.sort.bedgraph Gadd45.tko1.bismark.cov.major.egual.10.bed.bedgraph
-LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko2.bismark.cov.major.egual.10.bed.sort.bedgraph Gadd45.tko2.bismark.cov.major.egual.10.bed.bedgraph
-LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o Gadd45.tko3.bismark.cov.major.egual.10.bed.sort.bedgraph Gadd45.tko3.bismark.cov.major.egual.10.bed.bedgraph
-LC_ALL=C sort -T Temporary -k1,1 -k2,2n -o mESC.bismark.cov.major.egual.10.bed.sort.bedgraph mESC.bismark.cov.major.egual.10.bed.bedgraph
 
-#Correct coordinates
-for i in *.cov.major.egual.10.bed.sort.bedgraph;
+#Convert to BigWig
+./bedGraphToBigWig ES.WT.cov.major.egual.10.bed.bedgraph mm10.chr.sizes ES.WT.myCpG.cov.major.egual.10.bigWig &
+./bedGraphToBigWig G45.TKO1.cov.major.egual.10.bed.bedgraph mm10.chr.sizes G45.TKO1.cov.major.egual.10.bigWig &
+./bedGraphToBigWig G45.TKO2.cov.major.egual.10.bed.bedgraph mm10.chr.sizes G45.TKO2.cov.major.egual.10.bigWig &
+./bedGraphToBigWig G45.TKO3.cov.major.egual.10.bed.bedgraph mm10.chr.sizes G45.TKO3.cov.major.egual.10.bigWig &
+
+
+
+###Now for coverage >= 5
+
+#Filter CG with Coverage >= 10
+cat ES.WT.myCpG.bed | awk '$5 >= 5' > ES.WT.cov.major.egual.5.bed &
+cat G45.TKO1.myCpG.bed | awk '$5 >= 5' > G45.TKO1.cov.major.egual.5.bed &
+cat G45.TKO2.myCpG.bed | awk '$5 >= 5' > G45.TKO2.cov.major.egual.5.bed &
+cat G45.TKO3.myCpG.bed | awk '$5 >= 5' > G45.TKO3.cov.major.egual.5.bed &
+
+
+#Download bedGraphToBigWig
+
+wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bedGraphToBigWig
+
+
+#Convert the files in wig format #chr, start, end, meth	values
+for i in *egual.5.bed; 
 do
-       awk -F "\t" '{print $1"\t"$2-1"\t"$2"\t"$4}' $i > $i.coordinates;
+        cut -f 1,2,3,4 $i > $i.bedgraph; 
 done &
 
 
 #Convert to BigWig
-./bedGraphToBigWig Gadd45.tko1.bismark.cov.major.egual.10.bed.bedgraph.coordinates mm10.chr.sizes Gadd45.tko1.bismark.cov.major.egual.10.bed.bigWig &
-./bedGraphToBigWig Gadd45.tko2.bismark.cov.major.egual.10.bed.bedgraph.coordinates mm10.chr.sizes Gadd45.tko2.bismark.cov.major.egual.10.bed.bigWig &
-./bedGraphToBigWig Gadd45.tko3.bismark.cov.major.egual.10.bed.bedgraph.coordinates mm10.chr.sizes Gadd45.tko3.bismark.cov.major.egual.10.bed.bigWig &
-./bedGraphToBigWig mESC.bismark.cov.major.egual.10.bed.bedgraph.coordinates mm10.chr.sizes mESC.bismark.cov.major.egual.10.bed.bigWig
+./bedGraphToBigWig ES.WT.cov.major.egual.5.bed.bedgraph mm10.chr.sizes ES.WT.myCpG.cov.major.egual.5.bigWig &
+./bedGraphToBigWig G45.TKO1.cov.major.egual.5.bed.bedgraph mm10.chr.sizes G45.TKO1.cov.major.egual.5.bigWig &
+./bedGraphToBigWig G45.TKO2.cov.major.egual.5.bed.bedgraph mm10.chr.sizes G45.TKO2.cov.major.egual.5.bigWig &
+./bedGraphToBigWig G45.TKO3.cov.major.egual.5.bed.bedgraph mm10.chr.sizes G45.TKO3.cov.major.egual.5.bigWig &
