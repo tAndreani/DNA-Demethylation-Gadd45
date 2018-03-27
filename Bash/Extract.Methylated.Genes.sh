cf02d4539e1a66923cd56dd41d8e3934232a3d0b
Tommaso Andreani <tAndreani@users.noreply.github.com>
2018-03-27 12:20:27 +0200

    Update Extract.Methylated.Genes.sh

--- Bash/Extract.Methylated.Genes.sh
+++ Bash/Extract.Methylated.Genes.sh
@@ -1,3 +1,4 @@
 #From http://www.geneimprint.com/site/genes-by-species.Mus+musculus copy the genes and select only the imprinted ones
 
-grep Imprinted test2.save | grep Not -v > Imprinted.Genes.Mouse.txt
+
+grep Imprinted imprinted.and.not.imprinted.txt | grep Not -v > Imprinted.Genes.Mouse.txt
