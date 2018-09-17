b982ff0c5f0047017be1ad0b140f8f2dd5612b44
Tommaso Andreani <tAndreani@users.noreply.github.com>
2018-09-17 12:55:28 +0200

    Update custom_track_Neil.DKO.R

--- Custom_Track/custom_track_Neil.DKO.R
+++ Custom_Track/custom_track_Neil.DKO.R
@@ -1,12 +1,1 @@
-#Coverage >= 10 second approach
-track type=bigWig name='ES.WT.cov10.Neil' description='ES.WT.cov10.Neil' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=0,0,250 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/ES.WT.Neil.cov.major.egual.10.bigWig
-track type=bigWig name='Neil.DKO.8.cov10' description='Neil.DKO.8.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.8.cov.major.egual.10.bigWig
-track type=bigWig name='Neil.DKO.6.cov10' description='Neil.DKO.6.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.6.cov.major.egual.10.bigWig
-track type=bigWig name='Neil.DKO.7.cov10' description='Neil.DKO.7.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.7.cov.major.egual.10.bigWig
-
-
-#Coverage >= 5 second approach
-track type=bigWig name='ES.WT.cov5.Neil' description='ES.WT.cov5.Neil' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=0,0,250 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/ES.WT.Neil.cov.major.egual.5.bigWig
-track type=bigWig name='Neil.DKO.8.cov5' description='Neil.DKO.8.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.8.cov.major.egual.5.bigWig
-track type=bigWig name='Neil.DKO.6.cov5' description='Neil.DKO.6.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.6.cov.major.egual.5.bigWig
-track type=bigWig name='Neil.DKO.7.cov5' description='Neil.DKO.7.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/Neil.DKO.7.cov.major.egual.5.bigWig
+#Not available until submission
