c87f3d98a89f7de78d992b8a12aa9bdd6805f7b4
Tommaso Andreani <tAndreani@users.noreply.github.com>
2018-09-17 12:54:16 +0200

    Update custom_track_G45.R

--- Custom_Track/custom_track_G45.R
+++ Custom_Track/custom_track_G45.R
@@ -1,14 +1,1 @@
-#custom track:
-  
-#Coverage >= 10 
-track type=bigWig name='ES.WT.cov10' description='ES.WT.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=0,0,250 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/ES.WT.cov.major.egual.10.bigWig
-track type=bigWig name='Gadd45.TKO1.cov10' description='G45.TKO1.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO1.cov.major.egual.10.bigWig
-track type=bigWig name='Gadd45.TKO2.cov10' description='G45.TKO2.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO2.cov.major.egual.10.bigWig
-track type=bigWig name='Gadd45.TKO3.cov10' description='G45.TKO3.cov10' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO3.cov.major.egual.10.bigWig
-  
-  
-#Coverage >= 5
-track type=bigWig name='ES.WT.cov5' description='ES.WT.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=0,0,250 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/ES.WT.cov.major.egual.5.bigWig
-track type=bigWig name='Gadd45.TKO1.cov5' description='G45.TKO1.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO1.cov.major.egual.5.bigWig
-track type=bigWig name='Gadd45.TKO2.cov5' description='G45.TKO2.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO2.cov.major.egual.5.bigWig
-track type=bigWig name='Gadd45.TKO3.cov5' description='G45.TKO3.cov5' visibility="full" autoScale=off viewLimits=0:1 maxHeightPixels=32:32:11 color=250,0,0 bigDataUrl=http://imbc1.imb.uni-mainz.de/~tandrean/mm/G45.TKO3.cov.major.egual.5.bigWig
+#Not available until submission/acceptance
